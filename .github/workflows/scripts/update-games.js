f7802dae7c619d6c3c58ba87467ff4f31f23db23
glibbist <101940995+glibbist@users.noreply.github.com>
2025-07-11 22:22:17 -0400

    Update update-games.js

--- .github/workflows/scripts/update-games.js
+++ .github/workflows/scripts/update-games.js
@@ -1,6 +1,15 @@
 const axios = require('axios');
 const fs = require('fs');
 
+function loadExistingData() {
+    if (fs.existsSync('data.json')) {
+        const existing = fs.readFileSync('data.json', 'utf8');
+        return JSON.parse(existing);
+    } else {
+        return [];
+    }
+}
+
 async function fetchGames() {
     try {
         const response = await axios.get('https://pinballmap.com/api/v1/locations/17135/machine_details.json');
@@ -14,34 +23,57 @@ async function fetchGames() {
     }
 }
 
-function processGame(game) {
-    const yearClass = (() => {
-        if (game.year < 1980) return 0;
-        if (game.year <= 1997) return 1;
-        return 2;
-    })();
+function processGameData(inputGames,existingData) {
+    const allGames = inputGames.map(game => {
+        const existingEntry = existingData.find(e => e.id === game.id);
+        const yearClass = (() => {
+            if (game.year < 1980) return 0;
+            if (game.year <= 1997) return 1;
+            return 2;
+        })();
+        
+        // Create Common Name
+        const commonName = game.name.replace(/^The\s+/,'').replace(/(\(Gold\)|\s\(\d{4}\))/g,'').replace(/\s\(Pro\)|\s\(LE\)|\s\(Premium\)/,'');
+        
+        // Merge data
+        return {
+            id: game.id,
+            name: game.name,
+            year: game.year,
+            manufacturer: game.manufacturer,
+            ipdb_link: game.ipdb_link,
+            ipdb_id: game.ipdb_id,
+            kineticist_url: game.kineticist_url,
+            opdb_id: game.opdb_id,
+            commonName,
+            floor: (existingEntry ? existingEntry.floor : 1), // Keep existing floor or default to 1
+            yearClass, // Calculate only if new entry
+        };
+    });
 
-    return {
-        id: game.id,
-        name: game.name.replace(/(\d{4})\s|\s\(\w+\)/g, ''), // Remove (year) or (model) etc. 
-        year: game.year,
-        manufacturer: game.manufacturer,
-        ipdb_link: game.ipdb_link,
-        ipdb_id: game.ipdb_id,
-        kineticist_url: game.kineticist_url,
-        opdb_id: game.opdb_id,
-        commonName: game.name,
-        floor: 1, // default to floor 1 for new items
-        yearClass
-    };
+    // Remove games no longer present in API request
+    existingData.forEach(game => {
+        if (!allGames.find(g => g.id === game.id)) {
+            allGames.push(game);
+        }
+    });
+
+    return allGames;
 }
 
-async function main() {
-    const fetchedGames = await fetchGames();
+async function main(){
+    // Get fresh data & existing data
+    const inputGames = await fetchGames();
+    const existingData = loadExistingData();
+
+    // Sync datasets
+    const combinedData = processGameData(inputGames,existingData);
+    combinedData.sort((a,b) => a.id > b.id ? 1 : a.id === b.id ? 0 : -1);
+
     fs.writeFileSync(
-        'data.json',
-        JSON.stringify(fetchedGames.map(game => processGame(game)), null, 2)
-    );
+        'data.json', 
+        JSON.stringify(combinedData,null,2)
+    );  
 }
 
 main();
